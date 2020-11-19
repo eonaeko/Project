d5f6998ec05e4aad9541bdb46b38a93a94acd3ac
eonaeko <eonaeko778@gmail.com>
2020-11-19 02:20:59 +0100

    Edit as 19/11/2020

--- JS/Script.js
+++ JS/Script.js
@@ -7,10 +7,32 @@
           }
         }
   function Functionnew() {
-    var y = document.getElementById("mySidenav");
-    if (y.style.display === "block") {
+    var mySidenav= document.getElementById("mySidenav")
+    var y = document.getElementById("HTML-nav");
+    var htmlNav= document.getElementById("ires-aside");
+    var gridCont = document.querySelector(".grid-cont");
+    var tableRemove = document.getElementById("table-remove");
+    if (mySidenav.style.display === "block") {
+      
       y.style.display = "none";
+      mySidenav.style.display="none"
+      gridCont.style.gridTemplateColumns="100%";
     } else {
-      y.style.display = "block";
+      mySidenav.style.display = "block"; 
+     mySidenav.style.height="100%";
+     gridCont.style.gridTemplateColumns="40% 60%";
+     y.style.display="block";
+     tableRemove.style.display="none";
+     y.style.height='100%';
+     y.style.margin="-14px";
+     y.style.fontSize="100%";
+     //tableRemove.style.display="none";
     }
   }
+  /*function Createsidenav(){
+    var mainContent = document.getElementsByClassName("HTML-nav");
+    var gridCont = document.querySelector(".grid-cont");
+    var SideNav = document.getElementById("mySidenav");
+    var htmlNav= document.getElementById("ires-aside");
+    console.log(SideNav)
+  }*/
