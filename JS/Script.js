ea401030e02488c9784c1f73d71b0be53b65b73e
eonaeko <eonaeko778@gmail.com>
2020-11-20 23:10:43 +0100

    Edit as of 20/11/2020

--- JS/Script.js
+++ JS/Script.js
@@ -1,3 +1,9 @@
+window.onscroll = function(){StickyTopNav()};
+var headerNav = document.getElementById("Header-Nav");
+var headerStiff = headerNav.offsetTop; 
+var headerH1 = document.querySelector("#Header-Nav div");
+var headerBar = document.querySelector(" #main-nav");
+var myLinks = document.querySelector("#myLinks");
   function myFunction() {
           var x = document.getElementById("myLinks");
           if (x.style.display === "block") {
@@ -6,44 +12,34 @@
             x.style.display = "block";
           }
         }
-  /*function Functionnew() {
-    var mySidenav= document.getElementById("mySidenav")
-    var y = document.getElementById("HTML-nav");
-    var htmlNav= document.getElementById("ires-aside");
-    var gridCont = document.querySelector(".grid-cont");
-    var tableRemove = document.getElementById("table-remove");
-    if (mySidenav.style.display === "block") {
-      
-      y.style.display = "none";
-      mySidenav.style.display="none"
-      gridCont.style.gridTemplateColumns="100%";
-    } else {
-      mySidenav.style.display = "block"; 
-     mySidenav.style.height="100%";
-     gridCont.style.gridTemplateColumns="40% 60%";
-     y.style.display="block";
-     tableRemove.style.display="none";
-     y.style.height='100%';
-     y.style.margin="-14px";
-     y.style.fontSize="100%";
-     //tableRemove.style.display="none";
-    }
-  }*/
-  /*function Createsidenav(){
-    var mainContent = document.getElementsByClassName("HTML-nav");
-    var gridCont = document.querySelector(".grid-cont");
-    var SideNav = document.getElementById("mySidenav");
-    var htmlNav= document.getElementById("ires-aside");
-    console.log(SideNav)
-  }*/
+  
   function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-    //document.getElementById("main").style.marginLeft = "250px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
   }
   
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-   // document.getElementById("main").style.marginLeft= "1em";
     document.body.style.backgroundColor = "white";
+  }
+  
+
+  function StickyTopNav(){
+    if (window.pageYOffset >= headerStiff ) {
+      headerNav.style.height="auto";
+      headerNav.style.position="sticky";
+      headerNav.style.top="0";
+      headerNav.style.display="flex"
+      headerH1.style.fontSize ="75%";
+      headerH1.style.textAlign ="left";
+      headerH1.style.marginTop ="20px";
+      headerH1.style.marginRight ="10px";
+      headerBar.style.margin="0"
+      myLinks.style.marginBottom="20px";
+    } else{
+      headerNav.style.height ="200px"
+      headerNav.style.display="block"
+      headerH1.style.display ="block";
+      headerH1.style.textAlign ="center";
+    }
   }
