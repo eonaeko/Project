0ba281ec0e2d09a506e26c965c272858309b6f94
eonaeko <eonaeko778@gmail.com>
2020-12-03 23:11:15 +0100

    as of 03/12/2020

--- JS/Script.js
+++ JS/Script.js
@@ -16,12 +16,12 @@ var gridCont = document.querySelector(".grid-cont");
   
   function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    //document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
   }
   
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.body.style.backgroundColor = "white";
+  //  document.body.style.backgroundColor = "white";
   }
   
 
